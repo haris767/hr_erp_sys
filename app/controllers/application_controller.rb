d43e5e884eba8a5e97c2c5f8e8e2d69efda28653
haris767 <hariharis767@gmail.com>
2025-04-22 23:29:12 +0500

    devise configured

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,25 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  layout :layout_by_resource
+
+  private
+
+  def layout_by_resource
+    if devise_controller? && controller_name == "sessions"
+      "login"
+    elsif devise_controller? && controller_name == "registrations"
+      "signup"
+    else
+      "application"
+    end
+  end
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name, :active, :department_id ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :name, :active, :department_id ])
+  end
 end
---
+++ db/migrate/20250422160057_add_fields_to_users.rb
@@ -0,0 +1,6 @@
+class AddFieldsToUsers < ActiveRecord::Migration[8.0]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :active, :boolean
+  end
+end
