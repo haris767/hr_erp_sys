6902a5bd186ad4964f8be0212168a3b3d2c2f8c0
haris767 <hariharis767@gmail.com>
2025-04-23 20:16:37 +0500

    remove hr/users_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "hr/users#index"
-
-  namespace :hr do
-    resources :users, only: [ :index ]  # Adds the route for the users index page
-  end
+  root "pages#index"
 end
